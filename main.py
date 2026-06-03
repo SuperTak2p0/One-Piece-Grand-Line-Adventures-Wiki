318fb75f4e84089ba64029dff31c9a57f6a01cb2
Akshat Tak <akshattak2607@gmail.com>
2026-06-03 07:04:05 +0000

    realeased combant abilities

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def main():
     print("Hello from repl-nix-workspace!")
     updater = wiki_updater.WikiUpdater()
     updater.release_feature(
-        feature_name="Races",
-        description="Choose your race and start your journey, with common and very rare races that each offer unique abilities and playstyles.",
-        release_date="2025-06-01",
+        feature_name="Combat Abilities",
+        description= "Unlock unique abilities and sharpen haki and devil fruit powers to get stronger.",
+        release_date="Apr 29, 2026",
         mod_version="1.2.0",
         under_editing=True
     )
